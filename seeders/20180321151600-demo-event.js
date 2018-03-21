1a4b77072717c6dd3d27c9c600e6b6d573a5afcc
Lachezar Todorov <lltodorov1@gmail.com>
2018-03-21 17:42:47 +0200

    added seeder events

--- seeders/20180321151600-demo-event.js
+++ seeders/20180321151600-demo-event.js
@@ -2,25 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
+    return queryInterface.bulkInsert('Events', [{
+        date: '2018-02-05 20:00:00', 
+        createdAt: new Date,
+        updatedAt: new Date,
       }], {});
-    */
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('Events', null, {});
   }
 };
