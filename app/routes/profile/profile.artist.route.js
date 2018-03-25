c1a8728605c4e2da413373bdf03180f234ec1781
dobritopalov@gmail.com <dobritopalov@gmail.com>
2018-03-25 13:39:54 +0300

    edited settings-user/artist,register,login,recovery,checkout viewpages. Added new mixins and fixed setup-routes to work with folders. Prestructured route folder as well as views folder

--- app/routes/profile/profile.artist.route.js
+++ app/routes/profile/profile.artist.route.js
@@ -6,14 +6,10 @@ const init = (app, data) => {
   const router = new Router();
 
   router
-  .get('/user', async (req, res) => {
-    const context = {};
-    res.render('./profile/user-settings', context);
-  })
   .get('/artist', async (req, res) => {
     const context = {};
-    res.render('./profile/artist-settings', context);
-  });    
+    res.render('./artist/artistpage', context);
+  });  
 
   app.use('/', router);
 
