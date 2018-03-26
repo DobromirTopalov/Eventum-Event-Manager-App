daf9d0b8d984124f626a583eff98006f718576fb
Lachezar Todorov <lltodorov1@gmail.com>
2018-03-26 21:36:01 +0300

    started post request in register form

--- app/routes/register/register.route.js
+++ app/routes/register/register.route.js
@@ -3,24 +3,29 @@ const {
 } = require('express');
 
 const init = (app, data) => { 
-  const router = new Router();
+    const router = new Router();
 
-  router
-  .get('/register', async (req, res) => {
-    const context = {};
-    res.render('./register/register', context);
-  })
-  .get('/register/user', async (req, res) => {
-    const context = {};
-    res.render('./register/user-register', context);
-  })
-  .get('/register/artist', async (req, res) => {
-    const context = {};
-    res.render('./register/artist-register', context);
-  });    
+    router
+    .get('/register', async (req, res) => {
+        const context = {};
+        res.render('./register/register', context);
+    })
+    .get('/register/user', async (req, res) => {
+        const context = {};
+        res.render('./register/user-register', context);
+    })
+    .get('/register/artist', async (req, res) => {
+        const context = {};
+        res.render('./register/artist-register', context);
+    })
 
-  app.use('/', router);
-}
+    app.post('/register/artist', function(req, res, next) {
+        let username = req.body.username;
+        console.log(username);
+        // window.alert(username);
+    });
+    app.use('/', router);
+    }
 
 module.exports = {
   init,
