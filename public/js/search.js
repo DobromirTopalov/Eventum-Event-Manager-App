0a607cd68486df667e1b3bfb8744c3094d76fd2f
dobritopalov@gmail.com <dobritopalov@gmail.com>
2018-03-29 20:59:02 +0300

    event overview + event search not fully working, ajax is missing

---
+++ public/js/search.js
@@ -0,0 +1,31 @@
+$(function() {
+  $("#keywords").on("change", function() {
+    var words = $("#keywords").html();
+    var criteria_1 = $('#categories').val();
+    var criteria_2 = $('#cities').val();
+
+    const infoObj = {
+      words: words,
+      category: criteria_1,
+      city: criteria_2,
+    };
+    
+    // STILL UNDER CONSTRUCTION
+    // AJAX is wrong here
+
+    $.ajax({
+      method: 'POST',
+      async: true,
+      url: '/search',
+      dataType: 'json',
+      data: infoObj,
+      error: function(error) {
+          console.log(error);
+      },
+      success: function(data) {
+        console.log(data);
+          // window.location.href = '/login';
+      },
+    });
+  });
+});
