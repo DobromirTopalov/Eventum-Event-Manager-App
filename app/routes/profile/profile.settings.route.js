bb7a2fcfb429ef9f5f09a81cfa288b2e177c5a66
Lachezar Todorov <lltodorov1@gmail.com>
2018-04-01 12:23:32 +0300

    eslint errors fixed

--- app/routes/profile/profile.settings.route.js
+++ app/routes/profile/profile.settings.route.js
@@ -3,73 +3,66 @@ const {
   } = require('express');
 const {
     User,
-    Artist
-} = require('../../../models/data-class')
-const path = require('path')
-const passport = require('passport');
-const UserController = require('../register/register.controller');
+    Artist,
+} = require('../../../models/data-class');
+
+const path = require('path');
 const multer = require('multer');
+
+const UserController = require('../register/register.controller');
 const EncryptController = require('../../../controllers/EncryptingController');
+
+const __dirname = path.resolve();
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname,  '..', '..', '..', 'public', 'uploads'))
+    destination: function(req, file, cb) {
+      cb(null, path.join(__dirname, '..', '..', '..', 'public', 'uploads'));
     },
-    filename: async function (req, file, cb) {
-      cb(null, await EncryptController.imageName(file.originalname + Date.now()) + path.extname(file.originalname))
-    }
+    filename: async function(req, file, cb) {
+        cb(null,
+            await EncryptController.imageName(file.originalname + Date.now())
+            + path.extname(file.originalname));
+    },
   });
-const upload = multer({storage: storage});
+
+const upload = multer({ storage: storage });
+
 const init = (app, data) => {
     const router = new Router();
-    let userController = new UserController(data); 
-    // let uploading = multer({
-    //     dest: __dirname + '/../../../public/uploads/',
-    //   });
+    const userController = new UserController(data);
     router
     .get('/settings', async (req, res, next) => {
         // if (!req.user) {
         //     return res.redirect('/login');
         //   }
         // let userID = await req.user.id;
-        let userID = 3;
+        const userID = 3;
 
         req.userInfo = await data.users.getUserInfoById(userID)
         req.userExtraInfo = await data.users.getUserExtraInfoById(userID)
-        account = Object.assign(req.userInfo, req.userExtraInfo );
+        const account = Object.assign(req.userInfo, req.userExtraInfo );
 
-        res.render('./profile/settings', {account});
+        res.render('./profile/settings', { account });
     })
-    // .post('/settings', async (req, res, next) => {
-    //     const userData = req.body;
 
-    //     let userID = await req.user.id;
-    //     try {
-    //         await userController.updateUser(userID, userData);
-    //     } catch (err) {
-    //         let someError = err;
-    //         res.status(400).json({"err": err.message});
-    //     }
-    //     res.status(200).json({"success":true});
-    // })
-    .post('/settings', [ upload.any(), async function(req, res, next) {
+    .post('/settings', [upload.any(), async function(req, res, next) {
         const userData = await req.body;
         //  if (!req.user) {
         //     return res.redirect('/login');
         //   }
         // let userID = await req.user.id;
 
-        if(Object.keys(userData).length!==0) {
-            let userID = 2;
+        if (Object.keys(userData).length!==0) {
+            const userID = 2;
             //     let userID = await req.user.id;
                 try {
                     await userController.updateUser(userID, userData);
                 } catch (err) {
-                    let someError = err;
-                    res.status(400).json({"err": err.message});
+                    // let someError = err;
+                    res.status(400).json({ 'err': err.message });
                 }
-                res.status(200).json({"success":true});
+                res.status(200).json({ 'success': true });
         } else {
-            var file = await req.files.file;
+            const file = await req.files.file;
             await console.log(await file);
             res.end();
         }
