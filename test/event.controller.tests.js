afc9bb0508ded9813e6bc7eb081cdf9675664a71
Lachezar Todorov <lltodorov1@gmail.com>
2018-04-03 16:16:13 +0300

    tests

--- test/event.controller.tests.js
+++ test/event.controller.tests.js
@@ -12,13 +12,30 @@ const data = {};
 const fakeEventData = {
     events: {
         findById(id) {
-            return eventArray.find((user) => user.id === id);
+            return eventArray.find((event) => event.id === id);
+        },
+        addEventCoverPic(id, photo) {
+            return null;
+        },
+    },
+    country: {
+        getAll() {
+            return null;
+        },
+    },
+    categories: {
+        getAllCategories() {
+            return null;
         },
     },
 };
-describe('UserController', () => {
-    // const emailSpy = sinon.stub(fakeUserData.users, 'findByEmail')
-    // .returns(null);
+let testEvent = {};
+describe('EventController', () => {
+    let getCategorySpy = sinon.stub(fakeEventData.categories, 'getAllCategories');
+    let getCountrySpy = sinon.stub(fakeEventData.country, 'getAll');
+    let getPhotoSpy = sinon.stub(fakeEventData.events, 'addEventCoverPic');
+        
+    
     // const usernameSpy = sinon.stub(fakeUserData.users, 'findByUsername')
     // .returns(null);
     // const profilePicSpy = sinon.stub(fakeUserData.users, 'updateUserProfilePic')
@@ -27,38 +44,146 @@ describe('UserController', () => {
     // .returns(null);
     // const fakes = sinon.collection;
     // let Model = null;
-    // beforeEach(() => {
-    //     Model = {
-    //         findByUsername: (object) => {},
-    //         findByEmail: (object) => {},
-    //         destroy: (object) => {},
-    //     };
+    beforeEach(() => {
+        
+        let Model = {
+            findByUsername: (object) => {},
+            findByEmail: (object) => {},
+            destroy: (object) => {},
+        };
 
-    //      data.users = new UserData(Model);
-    // });
-    // afterEach(function() {
-    //     fakes.restore();
-    //     emailSpy.reset();
-    //     usernameSpy.reset();
-    //     profilePicSpy.reset();
-    // });
+        data.events = new EventData(Model);
+        testEvent = {
+            title: '',
+            date: '',
+            time: '',
+            country: '',
+            city: '',
+            placeName: '',
+            address: '',
+            description: '',
+            category: '',
+            subcategory: '',
+            capacity: '',
+            price: '',
+
+        };
+    });
+    afterEach(function() {
+        getCategorySpy.reset();
+        getCountrySpy.reset();
+        getPhotoSpy.reset();
+        testEvent = {};
+       
+    });
     describe('createEvent', () => {
-        describe('when only username  is passed', () => {
-            it('expect to throw add email message', async () => {
-                const testUser = {};
-                testUser.username = 'Peter';
+        describe('when only title is passed', () => {
+            it('expect to throw add add date in req format message', async () => {
+                testEvent.date = '';
+                testEvent.title = 'myevent';
 
-                const controller = new UserController(fakeUserData);
+                const controller = new EventController(fakeEventData);
 
                 const func = async () => {
-                    return await controller.createUser(testUser);
+                    return await controller.createEvent(1, testEvent);
                 };
 
                 return await expect(func())
                 .to.eventually.be.rejected
-                .and.has.property('message', 'Invalid email length');
+                .and.has.property('message', 'Please add a date in the required format');
                 // .and.be.an.instanceOf(UserError.EmptyUsername);
             });
         });
     });
+
+    describe('when only date and title  is passed', () => {
+        it('expect to throw add email message', async () => {
+            
+            testEvent.date = '11/12/2018';
+            testEvent.title = 'myevent';
+
+            const controller = new EventController(fakeEventData);
+
+            const func = async () => {
+                return await controller.createEvent(1, testEvent);
+            };
+
+            return await expect(func())
+            .to.eventually.be.rejected
+            .and.has.property('message', 'Please add time in the required format');
+            // .and.be.an.instanceOf(UserError.EmptyUsername);
+        });
+    });
+    describe('getCountry', () => {
+        describe('no countries are in the database', () => {
+            it('expect to return empty', async () => {
+
+                const controller = new EventController(fakeEventData);
+                getCountrySpy.returns(null);
+                const func = async () => {
+                    return await controller.getCountries();
+                };
+
+                return await expect(func())
+                .to.eventually.be.fulfilled;
+            });
+        });
+    });
+
+    describe('getcategories', () => {
+        describe('no ca are in the database', () => {
+            it('expect to return empty', async () => {
+
+
+                getCategorySpy.returns(null);
+                const controller = new EventController(fakeEventData);
+
+                const func = async () => {
+                    return await controller.getCategories();
+                };
+
+                return await expect(func())
+                .to.eventually.be.fulfilled;
+
+            });
+        });
+    });
+    describe('getcategories', () => {
+        describe('no ca are in the database', () => {
+            it('expect to return empty', async () => {
+
+
+                getCategorySpy
+                .returns(1);
+                const controller = new EventController(fakeEventData);
+
+                const func = async () => {
+                    return await controller.getCategories();
+                };
+
+                return await expect(func())
+                .to.eventually.be.fulfilled.equals(1);
+
+            });
+        });
+    });
+    // describe('add cover photo ', () => {
+    //     describe('no ca are in the database', () => {
+    //         it('expect to return empty', async () => {
+
+    //             getPhotoSpy
+    //             .returns(null);
+    //             const controller = new EventController(fakeEventData);
+
+    //             const func = async () => {
+    //                 return await controller.addEventCoverPic(1, 'photo.jpg');
+    //             };
+
+    //             return await expect(func())
+    //             .to.eventually.be.fulfilled.equals(1);
+
+    //         });
+    //     });
+    // });
+    
 });
